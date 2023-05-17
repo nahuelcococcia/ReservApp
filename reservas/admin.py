af0f98d7791dc68a20d94d1782ff81eb7131d660
Rodrigo Reynaga <ronico041012@gmail.com>
2023-05-17 16:08:10 -0300

    modelo coordinador registrado en admin

--- reservas/admin.py
+++ reservas/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Coordinator
 
-# Register your models here.
+
+@admin.register(Coordinator)
+class CoordinatorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'lastname', 'dni_number', 'is_active')
+    search_fields = ['name', 'lastname']
+    list_filter = ['is_active']
+
