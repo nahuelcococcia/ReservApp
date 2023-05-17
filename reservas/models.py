68f680faf8966ef35bcecc9b5a7098a9077417cf
Franco <fran14korn@live.com>
2023-05-17 13:23:52 -0300

    Crear modelo de Cliente

--- reservas/models.py
+++ reservas/models.py
@@ -4,3 +4,9 @@ from django.db import models
 # Create your models here.
 class Modelo1(models.Model):
     campo1 = models.CharField(max_length=50)
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=55)
+    last_name = models.CharField(max_length=55)
+    is_active = models.BooleanField(default=True)
--- reservas/urls.py
+++ reservas/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import index
 
 urlpatterns = [
-    path('home/',index),
+    path('home/', index),
     
     
 ]
