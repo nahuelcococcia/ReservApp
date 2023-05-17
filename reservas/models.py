0d5226314128c7de0625c0624aa0584165fbafb7
cristian <cristiandevar@gmail.com>
2023-05-16 23:26:59 -0300

    Creacion del modelo Servicio

--- reservas/models.py
+++ reservas/models.py
@@ -4,3 +4,10 @@ from django.db import models
 # Create your models here.
 class Modelo1(models.Model):
     campo1 = models.CharField(max_length=50)
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    price = models.IntegerField()
+    is_active = models.BooleanField(default=True)
