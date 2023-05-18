0f50b6cbbf5d95e28ee41afb5774c3de0a6f623b
nahuelcococcia <nahuelcococcia@gmail.com>
2023-05-17 22:22:53 -0300

    registro admin empleado

--- reservas/admin.py
+++ reservas/admin.py
@@ -16,3 +16,9 @@ class ClientAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'last_name', 'is_active')
     search_fields = ('name', 'last_name')
     list_filter = ['is_active']
+
+@admin.register(Empleado)
+class EmpleadoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'apellido', 'numero_legajo', "activo")
+    search_fields = ('nombre', 'apellido')
+    list_filter = ['activo']
