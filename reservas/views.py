176986ecb1d5d7d66cc85e8ec6d3b67f1618b717
cristian <cristiandevar@gmail.com>
2023-05-18 21:16:46 -0300

    Creacion del view EmployeeUpdate, Tomando en cuenta que el modelo se llama Employee. Tambien se crearon las carpetas templates y los templates generales llamados 'base.html' y 'create_update_form.html' para no reutilizar codigo como lo habiamos practicado en la capacitacion.

--- reservas/views.py
+++ reservas/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
-def index(request):
-    return f"hola mundo"
+# Considero que el Form de empleado se llama EmployeeForm
+
+
+def employee_update_view(request, employee_id):
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('employees')
+    employee = Employee.objects.get(id=employee_id)
+    form = EmployeeForm(instance=employee)
+    return render(request, 'employee_update.html', {
+        'form': form,
+        'submit': 'Actualizar'
+    })
+
