84a2f4b62e10222b5de504ef485ddb6b4c440f85
cristian <cristiandevar@gmail.com>
2023-05-19 18:21:05 -0300

    Cambio de configuracion del idioma

--- reservas/models.py
+++ reservas/models.py
@@ -2,10 +2,6 @@ from django.db import models
 
 
 # Create your models here.
-class Modelo1(models.Model):
-    campo1 = models.CharField(max_length=50)
-
-
 class Service(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
@@ -18,11 +14,12 @@ class Client(models.Model):
     last_name = models.CharField(max_length=55)
     is_active = models.BooleanField(default=True)
 
-class Empleado(models.Model):
-    nombre = models.CharField(max_length=100)
-    apellido = models.CharField(max_length=100)
-    numero_legajo = models.IntegerField(default=0)
-    activo = models.BooleanField(default=True)
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    file_number = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
 
 
 class ReserveService(models.Model):
@@ -30,6 +27,6 @@ class ReserveService(models.Model):
     reservation_date = models.DateField()
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     # responsible = models.ForeignKey(Coordinador, on_delete=models.CASCADE)
-    employee = models.ForeignKey(Empleado, on_delete=models.CASCADE)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
     price = models.IntegerField(default=0)
