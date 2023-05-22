3ae07b567853fd40a8092137fa581c96d5bf9ddd
cristian <cristiandevar@gmail.com>
2023-05-22 17:22:02 -0300

    Creacion del view para desactivar un Empleado

--- reservas/urls.py
+++ reservas/urls.py
@@ -4,7 +4,6 @@ from reservas import views
 
 urlpatterns = [
     path('home/', views.index, name="home"),
-    path('employee/new/', views.employee_register), 
-    
-    
+    path('employee/new/', views.employee_register),
+    path('employee/desactivate/<int:employee_id>', views.employee_desactivate, name="employee-desactivate"),
 ]
--- reservas/views.py
+++ reservas/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import EmployeeForm
+from .models import Employee
 
 # Create your views here.
 def index(request):
@@ -16,3 +17,9 @@ def employee_register(request):
         'form': form,
          "submit": "Registrar Empleado"
          })
+
+
+def employee_desactivate(request, employee_id):
+    employee = Employee.objects.get(id=employee_id)
+    employee.is_active = False
+    return HttpResponse('<h1> Se desactivo con exito </h1>')
