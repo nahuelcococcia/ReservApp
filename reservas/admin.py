6623ed9abc465747741197f0795b6a1526c15ab4
cristian <cristiandevar@gmail.com>
2023-06-07 02:18:58 -0300

    Reorganizamos el admin.py, no modificamos mas que las posiciones de las clases

--- reservas/admin.py
+++ reservas/admin.py
@@ -5,12 +5,22 @@ from .models import Service, Client, ReserveService, Employee, Coordinator
 # La sentencia @admin.register(Model) es un decorador que registra un modelo en el panel del administrador
 # por lo tanto cada que aparezca dicha sentencia se estara registrando un modelo, la finalidad es visualizarlo
 # en la pantalla admin
-@admin.register(Service)
-class ServiceAdmin(admin.ModelAdmin):
-    # Definimos la lista de atributos del modelo Service que mostraremos en el panel
-    list_display = ('id', 'name', 'description', 'price', 'is_active')
-    # Definimos al atributo name como criterio de búsqueda
-    search_fields = ['name']
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    # Definimos la lista de atributos del modelo Employee que mostraremos en el panel
+    list_display = ('id', 'name', 'lastname', 'file_number', "is_active")
+    # Definimos  a los atributos name y lastname, como criterio de búsqueda
+    search_fields = ('name', 'lastname')
+    # Definimos al atributo is_active como criterio de filtrado
+    list_filter = ['is_active']
+
+
+@admin.register(Coordinator)
+class CoordinatorAdmin(admin.ModelAdmin):
+    # Definimos la lista de atributos del modelo Coordinator que mostraremos en el panel
+    list_display = ('id', 'name', 'lastname', 'dni_number', "created_at", "is_active")
+    # Definimos  a los atributos name y lastname, como criterio de búsqueda
+    search_fields = ('name', 'lastname')
     # Definimos al atributo is_active como criterio de filtrado
     list_filter = ['is_active']
 
@@ -25,12 +35,12 @@ class ClientAdmin(admin.ModelAdmin):
     list_filter = ['is_active']
 
 
-@admin.register(Employee)
-class EmpleadoAdmin(admin.ModelAdmin):
-    # Definimos la lista de atributos del modelo Employee que mostraremos en el panel
-    list_display = ('id', 'name', 'lastname', 'file_number', "is_active")
-    # Definimos  a los atributos name y lastname, como criterio de búsqueda
-    search_fields = ('name', 'lastname')
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    # Definimos la lista de atributos del modelo Service que mostraremos en el panel
+    list_display = ('id', 'name', 'description', 'price', 'is_active')
+    # Definimos al atributo name como criterio de búsqueda
+    search_fields = ['name']
     # Definimos al atributo is_active como criterio de filtrado
     list_filter = ['is_active']
 
@@ -42,15 +52,6 @@ class ReserveServiceAdmin(admin.ModelAdmin):
     # Definimos a los atributos client, employee, service y responsible como criterio de búsqueda
     search_fields = ['client', 'employee', 'service', 'responsible']
 
-
-@admin.register(Coordinator)
-class CoordinatorAdmin(admin.ModelAdmin):
-    # Definimos la lista de atributos del modelo Coordinator que mostraremos en el panel
-    list_display = ('id', 'name', 'lastname', 'dni_number', "created_at", "is_active")
-    # Definimos  a los atributos name y lastname, como criterio de búsqueda
-    search_fields = ('name', 'lastname')
-    # Definimos al atributo is_active como criterio de filtrado
-    list_filter = ['is_active']
 
 # Se puede también en list_display reemplazar en algunos casos por la siguiente sentencia
 # list_display = [field.name for field in MiModelo._meta.fields]
