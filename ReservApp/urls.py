c6df5b0ae0a7f558c0737c5cb93add9946c9dd9a
cristian <cristiandevar@gmail.com>
2023-06-07 01:46:38 -0300

    Documentamos Admin, agregamos imagenes y agregamos el item Admin en navbar y name en urls.py

--- ReservApp/urls.py
+++ ReservApp/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name="admin"),
     path('', include("reservas.urls")),
     path('api/', include("api.urls"))
 ]
